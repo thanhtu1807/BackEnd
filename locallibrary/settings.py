ee4fdb06c3c56b27a8e8e82f8081426d97c7c517
Thanh Tu <thanhtu180793@gmail.com>
2020-04-30 20:24:47 +0700

    allowed_hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0","localhost","serene-shore-73304.herokuapp.com"]
 
 
 # Application definition
